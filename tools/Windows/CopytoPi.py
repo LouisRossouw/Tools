55883ba5f0de3182d3f72b194d16e72c54cb29b5
LouisRossouw <lourou99@gmail.com>
2021-07-17 00:15:14 +0200

    added an option to delete script after it has completed its task

--- tools/Windows/CopytoPi.py
+++ tools/Windows/CopytoPi.py
@@ -1,7 +1,9 @@
 import os
 from time import sleep
 
-# scp filename pi@10.0.0.102:~/Desktop/path
+# Script to quickly copy files from windows to raspberry pi,
+# Copy script into directory with the files you want to copy over.
+# the win cmd it uses: scp path/filename pi@10.0.0.102:~/Desktop/path
 
 script_file = os.path.basename(__file__)
 directory = os.path.dirname(__file__)
@@ -12,6 +14,8 @@ raspberry_dir = '~/Desktop/Dump'
 files = os.listdir(directory)
 
 print ('copy to Raspberry Pi started')
+
+response = input(f'\nDelete {script_file} after it is done? Y/N : ')
 
 data = []
 
@@ -26,3 +30,9 @@ os.system(f'scp -r {all_files} {raspberry}{raspberry_dir}')
 print (f'Done copying: {all_files}')
 
 sleep(5)
+
+# Delete script if user inputs y/Y
+if response == 'y' or response == 'Y':
+    os.remove(script_file)
+else:
+    pass
