07686618edb30ed43e6a793fc5bf1e1ae7f1608a
LouisRossouw <80905013+LouisRossouw@users.noreply.github.com>
2021-08-29 11:56:59 +0200

    Update ADB_StartMusic.py

--- tools/Linux/ADB_StartMusic.py
+++ tools/Linux/ADB_StartMusic.py
@@ -1,17 +1,1 @@
-import os
-from time import sleep
-from scripts.ADB_functions import connect_device, start_music, LogIn
-
-
-ADB = os.system
-
-IP = '10.0.0.109'
-
-connect_device(IP)
-
-LogIn()
-
-start_music()
-
-
-# ADB('killall adb.exe')
+# Temp file
